5a24eabf11a8b8d1d1f8e53e8b9f4e09dd7f42a3
Jessica Basilio <jessica.limabasilio@gmail.com>
2025-05-29 16:20:16 -0300

    Filling the templates

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -33,6 +33,24 @@ const url = require('url');
 //console.log('This one gotta be first!');
 
 //***************** SERVER *******************// 
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+
+    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+    return output;
+}
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
             
@@ -43,18 +61,30 @@ const server = http.createServer((req, res) => {
 
     const pathName = req.url;
 
+    //OVERVIEW PAGE
     if (pathName === '/' || pathName === '/overview'){
-        res.end('This is OVERVIEW PAGE!');
+        res.writeHead(200, { 'Content-type': 'text/html'});
+
+        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el));
+        console.log(cardsHtml);
+
+        res.end(tempOverview);
+
+    //PPRODUCT PAGE
     } else if (pathName === '/product') {
         res.end('This is a PRODUCT PAGE!');
+    
+    //API
     } else if (pathName === '/api') {
         /*fs.readFile(`${__dirname}/dev-data/data.json`, 'utf-8', (err, data) => {
             const productData = JSON.parse(data);
             res.writeHead(200, { 'Content-type': 'application/json'});
             res.end(data);
         });*/
-        res.writeHead(200, { 'Content-type': 'application/json'});
+        res.writeHead(200, { 'Content-type': 'appoutput = lication/json'});
         res.end(data);
+
+    //NOT FOUND
     } else {
         res.writeHead(404, {
            'Content-type': 'txt/html',
