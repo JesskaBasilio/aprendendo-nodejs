01d528a50ecbde23af0ae6ff9e3256967ec73731
Jessica Basilio <jessica.limabasilio@gmail.com>
2025-05-29 11:25:15 -0300

    Building the Templates

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -33,17 +33,28 @@ const url = require('url');
 //console.log('This one gotta be first!');
 
 //***************** SERVER *******************// 
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+            
 
 const server = http.createServer((req, res) => {
     //console.log(req.url);
     //res.end('Page connected');
-    
+
     const pathName = req.url;
 
     if (pathName === '/' || pathName === '/overview'){
         res.end('This is OVERVIEW PAGE!');
     } else if (pathName === '/product') {
         res.end('This is a PRODUCT PAGE!');
+    } else if (pathName === '/api') {
+        /*fs.readFile(`${__dirname}/dev-data/data.json`, 'utf-8', (err, data) => {
+            const productData = JSON.parse(data);
+            res.writeHead(200, { 'Content-type': 'application/json'});
+            res.end(data);
+        });*/
+        res.writeHead(200, { 'Content-type': 'application/json'});
+        res.end(data);
     } else {
         res.writeHead(404, {
            'Content-type': 'txt/html',
@@ -56,4 +67,9 @@ const server = http.createServer((req, res) => {
 server.listen(8000, '127.0.0.1', () => {
     console.log('Listening to requests on port 8000');
 })
+
+/* API é um serviço do qual podemos solicitar alguns dados
+JSON é um formato de texto simples que se parece muito com o objeto JAVASCRIPT, 
+pode ter array's, esses dados (valores dentro array's) são o que a nossa API 
+enviará para o cliente  quando solicitado */
 
